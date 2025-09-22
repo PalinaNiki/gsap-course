b16138894907f6adaed1b8070e15814a6838e767
PalinaNiki <nexti.job@gmail.com>
2025-09-22 10:57:20 +0100

    First Commit

---
+++ 2.1a PRO/aboutme.js
@@ -0,0 +1,6 @@
+let firstName = 'Palina';
+let lastName = 'PolNikitsina';
+let age = 'secret';
+let city = 'Minsk';
+let hobby = 'AI';
+console.log('Hello, my name is', firstName, "my last name is", lastName, "I'm", age, "years old", "I live in", city, "and my hobby is", hobby);
