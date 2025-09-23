5b01aaa8e1332d3f6671719b401c1cdf89cd3f68
PalinaNiki <nexti.job@gmail.com>
2025-09-23 08:55:17 +0100

    First Commit

---
+++ 2.2a PRO/app.js
@@ -0,0 +1,41 @@
+// Number
+// String
+// Boolean
+// Null
+// Undefined
+// Object
+// Symbol
+// BigInt
+
+const age = 25; // Number
+console.log(typeof age);
+const BigInt = 9007199254741991n; // BigInt
+console.log(typeof BigInt);
+const firstName = 'John'; // String
+console.log(typeof firstName);
+
+let age2; // Undefined
+console.log(age2);
+
+const Object = { // Object
+    name: 'John',
+    age: 25,
+    isProgrammer: true // Boolean
+};
+console.log(Object);
+console.log(typeof Object);
+
+
+
+
+// операторы
+// + - * / ** % ++ --
+
+const num = 11;
+const num2 = 5;
+console.log(num + num2); // сложение
+console.log(num - num2); // вычитание
+console.log(num * num2); // умножение
+console.log(num / num2); // деление
+console.log(num ** num2); // возведение в степень
+console.log(num % num2); // остаток от деления
