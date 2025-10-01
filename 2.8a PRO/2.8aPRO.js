edbdc9a09d105a7f1f213dc3968417ab52f92fd2
PalinaNiki <nexti.job@gmail.com>
2025-10-01 11:52:00 +0100

    2.8a PRO

--- 2.8a PRO/2.8aPRO.js
+++ 2.8a PRO/2.8aPRO.js
@@ -4,12 +4,28 @@ const tabNum = document.querySelectorAll(".tab-num");
 
 console.log(tabMenu);
 console.log(tabContent);
+console.log(tabNum);
 
 tabMenu.forEach((tabLink, index) => {
   tabLink.addEventListener("click", () => {
+    // Убираем класс is-active у всех tab-link
+    tabMenu.forEach((link) => {
+      link.classList.remove("is-active");
+    });
+
+    // Добавляем класс is-active к текущему tab-link
+    tabLink.classList.add("is-active");
+
+    // Убираем класс active у всех tab-pane
     tabContent.forEach((tabItem) => {
       tabItem.classList.remove("active");
     });
+
+    // Добавляем класс active к текущему tab-pane
     tabContent[index].classList.add("active");
   });
+});
+
+tabNum.forEach((item, index) => {
+  item.textContent = index + 1;
 });
