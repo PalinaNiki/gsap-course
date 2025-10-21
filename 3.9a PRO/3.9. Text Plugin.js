f824266e42d3b956c35604e3eb0a9bfbfa2b0d9d
PalinaNiki <nexti.job@gmail.com>
2025-10-21 21:52:28 +0300

    3.9a PRO

--- 3.9a PRO/3.9. Text Plugin.js
+++ 3.9a PRO/3.9. Text Plugin.js
@@ -1,31 +1,46 @@
-// Draggable
+// Text Plugin
 
-const draggable = document.querySelector(".draggable");
-
-Draggable.create(".drag", {
-  // type: "y",
-  bounds: draggable,
-  inertia: true,
-
-  onRelease: function () {
-    console.log("drag ended");
-    gsap.to(".drag", { scale: 1 });
+const textAnimation = gsap.to(".text1", {
+  duration: 2,
+  text: {
+    value: "The text has been changed using the Text Plugin!",
+    newClass: "class2",
+    delimiter: " ",
   },
-  onPress: function () {
-    console.log("onPress");
-    gsap.to(".drag", { scale: 0.9 });
-  },
-  dragClickables: true,
+  ease: "none",
+  paused: true,
 });
 
-// Добавляем новый Draggable для элемента drag-2
-Draggable.create(".drag-2", {
-  type: "rotation",
-  inertia: true,
+document.querySelector(".play-text").addEventListener("click", () => {
+  textAnimation.play();
 });
 
-Draggable.create(".ball", {
-  type: "x",
-  bounds: draggable,
-  inertia: true,
+const textTl = gsap.timeline({
+  repeat: -1,
+  repeatDelay: 1,
+  yoyo: true,
+});
+
+textTl.to(".text2", {
+  duration: 2,
+  text: "Простой способ написать текст с анимацией.",
+});
+
+// Добавление анимации для изменения текста кнопки
+const button = document.querySelector(".button");
+const buttonText = document.querySelector(".btn");
+
+buttonText.textContent = "Открыть модальное окно";
+
+button.addEventListener("click", () => {
+  const newText =
+    buttonText.textContent === "Открыть модальное окно"
+      ? "Закрыть модальное окно"
+      : "Открыть модальное окно";
+
+  gsap.to(buttonText, {
+    duration: 0.5,
+    text: newText,
+    ease: "power1.inOut",
+  });
 });
